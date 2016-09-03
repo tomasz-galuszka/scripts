ac5cf94f7677186fe96534a8701f419a82caef3e
Tomasz <tomgal144@gmail.com>
2016-09-03 10:43:17 +0100

    Update java-installation-script.sh

--- java-installation-script.sh
+++ java-installation-script.sh
@@ -1,22 +1,17 @@
 # == CONFIGURATION == (copy to: /etc/profile)
-DESTINATION_DIR=/usr/lib/jvm
-JDK_VERSION_TMP=jdk1.8.0_45
-
-# == ENV VARIABLES == (copy to: /etc/profile)
-JAVA_HOME=${DESTINATION_DIR}/${JDK_VERSION_TMP}
+JAVA_HOME=/usr/lib/jvm/jdk
 PATH=$PATH:$HOME/bin:$JAVA_HOME/bin
 export JAVA_HOME
 export PATH
 
 # == INSTALLATION ==
-sudo update-alternatives --install "/usr/bin/java" "java" "${DESTINATION_DIR}/${JDK_VERSION_TMP}/bin/java" 1  && \
-sudo update-alternatives --install "/usr/bin/javac" "javac" "${DESTINATION_DIR}/${JDK_VERSION_TMP}/bin/javac" 1  && \
-sudo update-alternatives --install "/usr/bin/javaws" "javaws" "${DESTINATION_DIR}/${JDK_VERSION_TMP}/bin/javaws" 1  && \
-sudo update-alternatives --set java ${DESTINATION_DIR}/${JDK_VERSION_TMP}/bin/java  && \
-sudo update-alternatives --set javac ${DESTINATION_DIR}/${JDK_VERSION_TMP}/bin/javac  && \
-sudo update-alternatives --set javaws ${DESTINATION_DIR}/${JDK_VERSION_TMP}/bin/javaws
-
 source /etc/profile
+sudo update-alternatives --install "/usr/bin/java" "java" "${JAVA_HOME}/bin/java" 1  && \
+sudo update-alternatives --install "/usr/bin/javac" "javac" "${JAVA_HOME}/bin/javac" 1  && \
+sudo update-alternatives --install "/usr/bin/javaws" "javaws" "${JAVA_HOME}/bin/javaws" 1  && \
+sudo update-alternatives --set java ${JAVA_HOME}/bin/java  && \
+sudo update-alternatives --set javac ${JAVA_HOME}/bin/javac  && \
+sudo update-alternatives --set javaws ${JAVA_HOME}/bin/javaws
 
 java -version
 
